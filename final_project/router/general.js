67a543ffa66d82021c088583aa3d4d4f06af8ec7
Ibrahim Ahmed <ibrahim3hmedib@gmail.com>
2024-03-27 17:34:42 +0200

    Refactor book filtering by author and title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -35,14 +35,14 @@ public_users.get("/isbn/:isbn", function (req, res) {
 // Get book details based on author
 public_users.get("/author/:author", function (req, res) {
   let author = req.params.author;
-  const filteredBooks = books.filter((book) => book.author === author);
+  const filteredBooks = Object.values(books).filter((book) => book.author === author);
   res.send(filteredBooks);
 });
 
 // Get all books based on title
 public_users.get("/title/:title", function (req, res) {
   let title = req.params.title;
-  const filteredBooks = books.filter((book) => book.title === title);
+  const filteredBooks = Object.values(books).filter((book) => book.title === title);
   res.json(filteredBooks);
 });
 
