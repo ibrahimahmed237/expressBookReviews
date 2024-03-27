019b91bf231b63f6370a7767a50ccb495a4e90a3
Ibrahim Ahmed <ibrahim3hmedib@gmail.com>
2024-03-27 17:10:06 +0200

    Refactor authentication and session handling in index.js and auth_users.js

--- final_project/index.js
+++ final_project/index.js
@@ -1,22 +1,48 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
-const session = require('express-session')
-const customer_routes = require('./router/auth_users.js').authenticated;
-const genl_routes = require('./router/general.js').general;
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const session = require("express-session");
+const e = require("express");
+const customer_routes = require("./router/auth_users.js").authenticated;
+const genl_routes = require("./router/general.js").general;
 
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use(
+  "/customer",
+  session({
+    secret: "fingerprint_customer",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+app.use("/customer/auth/*", async function auth(req, res, next) {
+  if (req.session.authorization) {
+    let token = req.session.authorization["accessToken"];
+    try {
+      const decoded = await jwt.verify(token, "access");
+      let user = decoded.user;
+      if (user) {
+        return next();
+      } else {
+        return res.status(403).json({
+          message: "User not authenticated!",
+        });
+      }
+    } catch (err) {
+      return res.status(403).json({
+        message: "User not authenticated!",
+      });
+    }
+  }
 });
- 
-const PORT =5000;
+
+const PORT = 5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => console.log("Server is running"));
